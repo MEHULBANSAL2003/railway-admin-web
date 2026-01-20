306f1f4b69a28f91346c18951d406ae540807d4c
MEHULBANSAL2003 <mehulbansal83606@gmail.com>
2026-01-20 14:10:51 +0530

    login page design

--- src/pages/LoginPage/Login.jsx
+++ src/pages/LoginPage/Login.jsx
@@ -1,9 +1,32 @@
-
+import './Login.css';
 
 const Login = () => {
   return (
-    <></>
-  )
+    <div className="login-container">
+      <div className="login-card">
+
+        {/* Railway illustration */}
+        <div className="railway-illustration">
+          🚆
+        </div>
+
+        <h1 className="login-title">Railways</h1>
+        <p className="login-subtitle">
+          Sign in to start session
+        </p>
+
+        {/* Google Login Button */}
+        <button className="google-login-btn">
+          <img
+            src="https://developers.google.com/identity/images/g-logo.png"
+            alt="Google"
+          />
+          Login with Google
+        </button>
+
+      </div>
+    </div>
+  );
 };
 
 export default Login;
