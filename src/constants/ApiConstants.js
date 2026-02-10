cfc648615aa5131b46f87631751e10490f1eb9ca
MEHULBANSAL2003 <mehulbansal83606@gmail.com>
2026-02-10 22:09:55 +0530

    station service setup

--- src/constants/ApiConstants.js
+++ src/constants/ApiConstants.js
@@ -3,5 +3,7 @@ export const ApiConstants = {
   adminEmailLogin: '/admin/login/by/email',
   refreshAccessToken: '/refresh/access/token',
   logoutCurrentDevice: '/admin/logout',
-  createNewAdmin: '/new/admin/create'
+  createNewAdmin: '/new/admin/create',
+
+  createNewStation: '/admin/add/new/station',
 }
---
+++ src/services/StationService.js
@@ -0,0 +1,13 @@
+import {ApiConstants} from "../constants/ApiConstants.js";
+import {HttpWrapper} from "../httpWrapper/HttpWrapper.js";
+
+
+const station_base_url =  import.meta.env.VITE_API_STATION_URL;
+
+export const StationService = {
+
+  createNewStation : async(payload) => {
+    const url = `${station_base_url}${ApiConstants.createNewStation}`;
+    return await HttpWrapper.post(url, payload);
+  },
+}
