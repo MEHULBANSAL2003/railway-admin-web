73274f687f83b0ea4e2601d44f94e0435a4812aa
MEHULBANSAL2003 <mehulbansal83606@gmail.com>
2026-02-27 15:01:18 +0530

    remake

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -3,12 +3,10 @@ import { Routes, Route, Navigate } from 'react-router-dom';
 import ProtectedRoute from "./ProtectedRoute.jsx";
 import PublicRoute from "./PublicRoute.jsx";
 import MainLayout from "../layout/MainLayout.jsx";
-import CreateAdmin from "../pages/createAdmin/CreateAdmin.jsx";
 
 
 const Login = lazy(() => import('../pages/LoginPage/Login.jsx'));
 const Dashboard = lazy(() => import('../pages/DashBoard/DashBoard.jsx'));
-const StationManagement = lazy(() => import('../pages/stationManagement/StationList.jsx'));
 
 const AppRoutes = () => {
   return (
@@ -32,8 +30,6 @@ const AppRoutes = () => {
       >
         <Route index element={<Navigate to="/dashboard" replace />} />
         <Route path="dashboard" element={<Dashboard />} />
-        <Route path='admin/create' element={<CreateAdmin/>}/>
-        <Route path='/admin/stations' element={<StationManagement/>} />
       </Route>
 
       <Route path="/" element={<Navigate to="/dashboard" replace />} />
